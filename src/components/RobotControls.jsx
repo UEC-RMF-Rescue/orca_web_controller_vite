d887a39edf6f63bad1f54e6584d62c9d613e6672
Shunsuke Nakamachi <shunsuke.040222@gmail.com>
2025-06-26 11:54:03 +0000

    add execute and terminate

--- src/components/RobotControls.jsx
+++ src/components/RobotControls.jsx
@@ -1,20 +1,68 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import ROSLIB from 'roslib';
-import "../styles/RobotControls.css"
+import '../styles/RobotControls.css';
 
-export default function RobotControls({robotName, ros}){
-    return (
-        <div className='wrapper'>
-            <div className='button-list'>
-                <div className='posctl-buttonlist'>
-                    <img src="../../public/target32.jpeg" alt="stylesheet"></img>
-                    <img src="../../public/start32.jpeg" alt="stylesheet"></img>
-                    <img src="../../public/trash32.jpeg" alt="stylesheet"></img>
-                </div>
-                <div className='show-rad'>
-                    <p>rad</p>
-                </div>
-            </div>
+export default function RobotControls({ robotName, ros }) {
+  const [yawOffset, setYawOffset] = useState(null);
+
+  useEffect(() => {
+    if (!ros || !robotName) return;
+
+    const topic = new ROSLIB.Topic({
+      ros,
+      name: `/${robotName}/yaw_rad_offset`,
+      messageType: 'std_msgs/Float64'
+    });
+
+    topic.subscribe((message) => {
+      setYawOffset(message.data.toFixed(2));
+    });
+
+    return () => {
+      topic.unsubscribe();
+    };
+  }, [ros, robotName]);
+
+  const callService = (serviceName) => {
+    const service = new ROSLIB.Service({
+      ros,
+      name: `/${robotName}/${serviceName}`,
+      serviceType: 'std_srvs/Empty'
+    });
+
+    const request = new ROSLIB.ServiceRequest();
+    service.callService(request, () => {
+      console.log(`${serviceName} called for ${robotName}`);
+    });
+  };
+
+  return (
+    <div className='wrapper'>
+      <div className='button-list'>
+        <div className='posctl-buttonlist'>
+          <img
+            src="../../public/target32.jpeg"
+            alt="target"
+            className='icon-button'
+            onClick={() => console.log(`Select ${robotName} for marker input`)}
+          />
+          <img
+            src="../../public/start32.jpeg"
+            alt="start"
+            className='icon-button'
+            onClick={() => callService('execute_posctl')}
+          />
+          <img
+            src="../../public/trash32.jpeg"
+            alt="stop"
+            className='icon-button'
+            onClick={() => callService('terminate_posctl')}
+          />
         </div>
-    )
+        <div className='show-rad'>
+          <p>{yawOffset !== null ? `deg: ${yawOffset}` : 'deg: --'}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
